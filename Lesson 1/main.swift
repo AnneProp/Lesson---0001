1ffef495af56d0a945a0fd748ea9787fbc80c775
Minh <Perfectrblx546@gmail.com>
2024-11-23 17:19:37 +0700

    Giới thiệu về bản thân

--- Lesson 1/main.swift
+++ Lesson 1/main.swift
@@ -9,4 +9,8 @@ import Foundation
 
 print("Hello, World!")
 
-print("123")
+print(123)
+print("Tên: Trịnh Ngọc Minh")
+print("Lớp: lớp 9")
+print("trường: Nguyễn Siêu")
+print("Hiện tại ở trên trường con đang theo học trường trình IGCSE kết hợp giữa cambridge và Việt Nam")
